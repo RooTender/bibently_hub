6d264c46f396b3a6fb239e9305b38adfacddafee
hlewandowski <hubert.lewandowski@pg.edu.pl>
2026-02-06 15:55:32 +0100

    Remove Caddy

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-NETWORK_NAME="bibently_web"
-
-if ! docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
-  echo "Creating docker network: $NETWORK_NAME"
-  docker network create "$NETWORK_NAME"
-else
-  echo "Docker network already exists: $NETWORK_NAME"
-fi
-
 echo "Starting Caddy..."
 docker compose up -d
 
